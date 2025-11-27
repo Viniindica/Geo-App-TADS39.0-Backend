860c2bf7524021281e366bbae0d8b3cc1c9363e7
Vinicius Cândido <158461196+Viniindica@users.noreply.github.com>
2025-11-26 23:44:58 -0300

    Remove hardcoded MongoDB URI
    Remove hardcoded MongoDB URI for security reasons.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Place = require('./models/Place');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://viniciuscandidonader_db_user:ekUpM5U5p6ymRy0t@cluster0.mnnmq5s.mongodb.net/?appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI || '';
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
